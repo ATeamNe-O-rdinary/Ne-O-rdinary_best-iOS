4fb0b08919c65c99017c4208c0d130059c915a96
Gyeongbin Im <hsmikvyv@gmail.com>
2025-11-23 00:05:22 +0900

    feat: swiftUI 폰트, hex 컬러 추가

--- Ne-O-rdinary_best-iOS/DesignSystem/HexString.swift
+++ Ne-O-rdinary_best-iOS/DesignSystem/HexString.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import UIKit
+import SwiftUI
 
 extension UIColor {
     convenience init(hexString: String, alpha: CGFloat = 1.0) {
@@ -25,6 +25,28 @@ extension UIColor {
         let green = CGFloat(g) / 255.0
         let blue  = CGFloat(b) / 255.0
         self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
+extension Color {
+    init(hex: String, alpha: CGFloat = 1.0) {
+        // 공백/줄바꿈 제거
+        let hex = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hex)
+        
+        // # 있으면 건너뛰기
+        if hex.hasPrefix("#") {
+            scanner.currentIndex = hex.index(after: hex.startIndex)
+        }
+        
+        var color: UInt64 = 0
+        scanner.scanHexInt64(&color)
+        
+        let r = Double((color & 0xFF0000) >> 16) / 255.0
+        let g = Double((color & 0x00FF00) >> 8) / 255.0
+        let b = Double(color & 0x0000FF) / 255.0
+        
+        self.init(.sRGB, red: r, green: g, blue: b, opacity: Double(alpha))
     }
 }
 
